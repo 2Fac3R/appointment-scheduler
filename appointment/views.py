52e9089d3ac7311ef3ea467a23e07c7c74e8d332
Gustavo Navarro <rockeg18@gmail.com>
2021-12-03 13:08:22 -0600

    Appointment: views, urls

--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework import permissions
+from django.contrib.auth.models import User, Group
 
-# Create your views here.
+from .serializers import UserSerializer, GroupSerializer, AppointmentSerializer
+from .models import Appointment
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('id')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows appointments to be viewed or edited.
+    """
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+class AppointmentList(generics.ListAPIView):
+    serializer_class = AppointmentSerializer
+
+    def get_queryset(self):
+        return Appointment.objects.filter(user=self.request.query_params.get('id', None))
