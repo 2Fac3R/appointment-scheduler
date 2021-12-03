1989eb9fd6be20425e2570a44a3cfb3835c537b7
Gustavo Navarro <rockeg18@gmail.com>
2021-12-03 12:53:59 -0600

    Appointment Model and its registration to admin.py

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    """Model representing an appointment"""
+    date = models.DateTimeField(null=False, blank=False)
+    user = models.ManyToManyField(User)
+    
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return str(self.date.strftime(settings.DATE_TIME_FIELD_FORMAT))
+        
