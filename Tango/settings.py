b359933b03ef843166b8993ed142ae6f917f30ee
Gustavo Navarro <rockeg18@gmail.com>
2021-12-03 12:46:12 -0600

    Installation of appointments app and DjangoRestFramework

--- Tango/settings.py
+++ Tango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'appointment.apps.AppointmentConfig', # appointment/apps.py
+    'rest_framework', #djangorestframework
 ]
 
 MIDDLEWARE = [
